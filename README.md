2e9964f59370656dfec29459b82cb5d3679c3676
Fabio <fabiobenoit@hotmail.fr>
2017-03-14 10:46:08 +0100

    Added Readme, browser index and working crawler

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# crawler
+# Web Crawler
+
+> Node.js basic web crawler
+
+
+## Installation
+
+1. Download / clone the project and extract the folder from the .zip
+2. If you don't already have it : [INSTALL NODE](https://nodejs.org/en/)
+3. Open command prompt in the directory (if you are on windows : Shift + Right Click -> 'Open command window here')
+4. Type in the console `npm install`
+
+You now have all the files needed !
+
+## How to use
+
+The program includes a really basic user interface. Once the node is launched, you won't have to type anything into the command prompt to have a result. Everything else has to be done in your browser.
+
+Steps:
+1. Run `node crawler.js` in the command prompt to launch the node
+If everything works, you should see the message 'App listening on port 8000. Open localhost:8000 in your browser'
+__WARNING__ : Never close the command prompt window !! (or the node will be stopped)
+2. Open your browser and go to the adress [localhost:8000](localhost:8000)
+3. Here, enter the website URL you want to crawl in the textbox (make sure there is no mistake in the URL, as there is almost no error handling)
+___Good URL format example :___ `https://www.google.com` (don't forget the https:// or the request might fail)
+4. Hit the 'Crawl !' button
+5. Nothing happens on the browser. That's normal. Now take a look at your command prompt window. The JSON object with every reachable page and every static asset should appear !
+
+## Important things
+
+- For unknown reasons, some scripts assets are not considered by the crawler, so they might not appear in the output.
+- As requested, the crawler does not cross subdomains and doesn't pick external links (social media links present on the page for example)
+- Crawler made for [GoCardless](https://www.gocardless.com)
--- crawler.js
+++ crawler.js
@@ -1,52 +1,199 @@
-//import modules
+//import required modules
 var express = require( 'express' );
 var request = require( 'request' );
 var cheerio = require( 'cheerio' );
 
-var urls = [];
-var websiteUrl = 'https://gocardless.com';
-if(websiteUrl[websiteUrl.length-1] == '/'){
-	console.log("lol");
-}
+
 //creating a new express server
 var app = express();
 
 //setting EJS as the templating engine
 app.set( 'view engine', 'ejs' );
 
-//setting the 'assets' directory as our static assets dir (css, js, img, etc...)
-app.use( '/assets', express.static( 'assets' ) );
-
-
-//makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
+//makes the server respond to the '/' route and serving the 'index.ejs' file in the 'views' directory
 app.get( '/', function ( req, res ) {
-    res.render( 'home', {
-        message: 'Home Page'
-    });
+	res.render( 'index', {
+ 		error: "",
+     });
 });
+
+
+//'main' function : get the value of the url entered by the user and then call the other functions
+app.get( '/process', function ( req, res ) {
+
+	//while getting the url, if the user left the last slash of the url, remove it with regex
+	const rootURL = req.query.webURL.replace(/\/$/, "");
+	if (rootURL) {
+
+		//call the function getWebSites if the user well entered a url
+		getWebSites(rootURL, insertUrls);
+	}
+});
+
+
 
 //launch the server on the 8000 port
 app.listen( 8000, function () {
-    console.log( 'App listening on port 8000. Open localhost/8000 on your browser' );
+    console.log( 'App listening on port 8000. Open localhost:8000 in your browser' );
 });
 
-request(websiteUrl, function(err, resp, body){
-	if(!err && resp.statusCode == 200) {
-		var $ = cheerio.load(body);
 
-		//Getting all the links 'a' from the webpage
-		$('a').each(function(){
+//initialize the JSON array we will output
+var websitesJSON = [];
 
-			//Getting the href attribute from the 'a' link
-			var url = $(this).attr('href');
+/*
+This var is a little bit special : as I am using a lot of callbacks in my code, due to the asynchronous nature of JS,
+we can't know exactly when the websitesJSON array will be fully completed. To solve this problem, I created a counter
+I will increment the number of times I use callbacks, and decrement it when the callback is launched.
+Finally, when the callbackCount goes back to 0, all the callbacks functions are supposed to be called, and the program
+will output the JSON array (see the function insertAssets for more details)
+*/
+var callbackCount = 0;
 
-			//We keep the link only if it is the same root (in order to avoid the 'undefined' links and the subdomains or outside links (like social media links))
-			if(url != undefined && url[0] == '/') {
-				url = websiteUrl + url;
-				urls.push(url);
-			}
-			
-		});
-		console.log(urls);
+
+//get all the links from the root webpage
+function getWebSites(rootURL, callback){
+
+	var urlsArray = []; //array where we will stock the links on the page
+
+	//Request function to get the whole html page
+	request(rootURL, function(err, resp, html){
+		if(!err && resp.statusCode == 200) {
+
+			//Use the cheerio module to load the html and then use JQuery functions
+			var $ = cheerio.load(html);
+
+			//Getting all the links 'a' from the webpage
+			$('a').each(function(){
+
+				//Getting the href attribute from the 'a' link
+				var url = $(this).attr('href');
+
+				//We keep the link only if it is the same root (in order to avoid the 'undefined' links and the subdomains or outside links (like social media links))
+				if(url != undefined && url[0] == '/') {
+
+					//We add the domain name to the url we got in order to have the full link
+					url = rootURL + url;
+
+					urlsArray.push(url); //push to the array
+				}
+				
+			});
+		}
+
+		//Call the function insertUrls
+		callback(urlsArray, getAssets, rootURL);
+	});
+}
+
+//Insert urls into the websitesJSON array
+function insertUrls(urlsArray, callback, rootURL){
+
+	callbackCount = urlsArray.length;
+
+	//Create a new row in the JSON array for every link we got in the previous function
+	for(var i = 0; i<urlsArray.length; i++){
+		websitesJSON.push({});
+		websitesJSON[i].url = urlsArray[i];
 	}
-});
+
+	//Get the assets and then add them to the JSON array
+	for(var i = 0; i < urlsArray.length; i++){
+		callback(websitesJSON[i].url, insertAssets, i, rootURL);
+	}
+
+	
+}
+
+
+//Get the assets from a specific link
+function getAssets(webUrl, callback, iteration, rootURL) {
+
+	var assetsArray = []; //array where we will stock the links of the assets
+
+	request(webUrl, function(err, resp, html) {
+		if(!err && resp.statusCode == 200) {
+			var $ = cheerio.load(html);
+
+			//Getting all the scripts from the webpage
+			$('script').each(function(){
+
+				//Getting the src attribute
+				var url = $(this).attr('src');
+
+				//Checks if the url exists
+				if(url != undefined) {
+
+					//Checks if the url has an internal link (to add the root URL to it if so)
+					if(url[0] == '/' && url[1] != '/'){
+						url = rootURL + url;
+						assetsArray.push(url);
+					}
+					else {
+						assetsArray.push(url);
+					}
+				}
+			});
+
+
+			//Getting all the images from the webpage
+			$('img').each(function(){
+
+				//Getting the src attribute
+				var url = $(this).attr('src');
+
+				//Checks if the url exists
+				if(url != undefined) {
+
+					//Checks if the url has an internal link (to add the root URL to it if so)
+					if(url[0] == '/' && url[1] != '/'){
+						url = rootURL + url;
+						assetsArray.push(url);
+					}
+					else {
+						assetsArray.push(url);
+					}
+				}
+			});
+
+			//Getting all the css from the webpage
+			$('link[rel="stylesheet"]').each(function(){
+
+				//Getting the href attribute
+				var url = $(this).attr('href');
+
+				//Checks if the url exists
+				if(url != undefined) {
+
+					//Checks if the url has an internal link (to add the root URL to it if so)
+					if(url[0] == '/' && url[1] != '/'){
+						url = rootURL + url;
+						assetsArray.push(url);
+					}
+					else {
+						assetsArray.push(url);
+					}
+				}
+			});
+
+		}
+
+		//Call the function insertAssets
+		callback(assetsArray, iteration);
+	});
+}
+
+//Insert the assets in the JSON array
+function insertAssets(assetsArray, iteration) {
+
+	//Set the 'assets' property in the JSON 
+	websitesJSON[iteration].assets = assetsArray;
+
+	//Decrement the callback counter because the callback has finished
+	callbackCount--;
+
+	//When evey callback is finished (i.e. callbackCount=0), STDOUT the JSON array
+	if(callbackCount === 0) {
+		console.log(websitesJSON);
+	}
+}
